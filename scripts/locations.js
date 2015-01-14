1c89a1022038ac6456e91c706ecbc4a6084350b9
David Dugan <DavidADugan@gmail.com>
2015-01-13 23:59:51 -0600

    Creating location list

--- scripts/locations.js
+++ scripts/locations.js
@@ -13,5 +13,130 @@
 		'name': 'The Traveling Circus'
 		'id': 'circus'
 		'street' 'fields'
+	},
+	{
+		'name':'Adminstration Building'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name':'Arkham Asylum'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name':'Bank of Arkham'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'Black Cave'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'Curiosite Shoppe'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'General Store'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'Graveyard'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'Hibb\'s Roadhouse'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'Historical Society'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'Independence Square'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'Library'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'Ma\'s Boarding House'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'Newspaper'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'Police Station'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'River Docks'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'Science Building'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'South Church'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'St. Mary\'s Hospital'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'The Unnamable'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'The Witch House'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'Train Station'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'Unvisited Isle'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'Velma\'s Diner'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'Woods'
+		'id': ''
+		'street' ''
+	},
+	{
+		'name': 'Ye Olde Magick Shoppe'
+		'id': ''
+		'street' ''
 	}
-}	
+}
