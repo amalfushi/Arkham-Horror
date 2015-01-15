836cee133da6f215d49e251c88811c002adbbc5b
David Dugan <DavidADugan@gmail.com>
2015-01-14 23:46:08 -0600

    Temporary locations file list, need to move into service call

--- scripts/app.js
+++ scripts/app.js
@@ -11,27 +11,11 @@
         'body': "Wandering through the Mirror Maze you find yourself laughing at the characiture's of yourself that the mirrors reveal. That is until you realize that some of these images seem to not only be different shapes than normal, but also seem to wear slightly different clothing and bear slightly different scars. As if they were not contorted mirrors but rather other versions of yourself, other possibilities."
       }];
 
-      $scope.locations = [
-        {
-           'name': 'Silver Moon Lodge',
-          'id': 'lodge',
-          'street': 'blue'
-        },
-        {
-          'name': 'Inner Sanctum',
-          'id': 'sanctum',
-          'street': 'blue'
-        },
-        {
-          'name': 'The Traveling Circus',
-          'id': 'circus',
-          'street': 'fields'
-        }];
-
+      $scope.locations = locations; 
       $scope.getEncounter = function(loc){
         //event.preventDefault();
         $scope.locale = loc;
-        $scope.encounter = $scope.circus[0]; 
+        $scope.encounter = $scope.circus[0];
       }
   });
 })();
--- scripts/locations.js
+++ scripts/locations.js
@@ -1,4 +1,5 @@
-{
+
+	var locations = [
 	{
  		'name': 'Silver Moon Lodge',
 		'id': 'lodge',
@@ -10,133 +11,133 @@
 		'street': 'blue'
 	},
 	{
-		'name': 'The Traveling Circus'
-		'id': 'circus'
+		'name': 'The Traveling Circus',
+		'id': 'circus',
 		'street': 'fields'
 	},
 	{
-		'name':'Adminstration Building'
-		'id': 'admin'
+		'name':'Adminstration Building',
+		'id': 'admin',
 		'street': 'miskatonic'
 	},
 	{
-		'name':'Arkham Asylum'
-		'id': 'bank'
+		'name':'Arkham Asylum',
+		'id': 'bank',
 		'street': 'downtown'
 	},
 	{
-		'name':'Bank of Arkham'
-		'id': 'bank'
+		'name':'Bank of Arkham',
+		'id': 'bank',
 		'street': 'downtown'
 	},
 	{
-		'name': 'Black Cave'
-		'id': 'cave'
+		'name': 'Black Cave',
+		'id': 'cave',
 		'street': 'rivertown'
 	},
 	{
-		'name': 'Curiositie Shoppe'
-		'id': 'curiositie'
+		'name': 'Curiositie Shoppe',
+		'id': 'curiositie',
 		'street': 'northside'
 	},
 	{
-		'name': 'General Store'
-		'id': 'general'
+		'name': 'General Store',
+		'id': 'general',
 		'street': 'rivertown'
 	},
 	{
-		'name': 'Graveyard'
-		'id': 'graveyard'
+		'name': 'Graveyard',
+		'id': 'graveyard',
 		'street': 'rivertown'
 	},
 	{
-		'name': 'Hibb\'s Roadhouse'
-		'id': 'hibb'
+		'name': 'Hibb\'s Roadhouse',
+		'id': 'hibb',
 		'street': 'easttown'
 	},
 	{
-		'name': 'Historical Society'
-		'id': 'historical'
+		'name': 'Historical Society',
+		'id': 'historical',
 		'street': 'southside'
 	},
 	{
-		'name': 'Independence Square'
-		'id': 'independence'
+		'name': 'Independence Square',
+		'id': 'independence',
 		'street': 'downtown'
 	},
 	{
-		'name': 'Library'
-		'id': 'library'
+		'name': 'Library',
+		'id': 'library',
 		'street': 'miskatonic'
 	},
 	{
-		'name': 'Ma\'s Boarding House'
-		'id': 'ma'
+		'name': 'Ma\'s Boarding House',
+		'id': 'ma',
 		'street': 'southside'
 	},
 	{
-		'name': 'Newspaper'
-		'id': 'newspaper'
+		'name': 'Newspaper',
+		'id': 'newspaper',
 		'street': 'northside'
 	},
 	{
-		'name': 'Police Station'
-		'id': 'police'
+		'name': 'Police Station',
+		'id': 'police',
 		'street': 'easttown'
 	},
 	{
-		'name': 'River Docks'
-		'id': 'docks'
+		'name': 'River Docks',
+		'id': 'docks',
 		'street': 'merchant'
 	},
 	{
-		'name': 'Science Building'
-		'id': 'science'
+		'name': 'Science Building',
+		'id': 'science',
 		'street': 'miskatonic'
 	},
 	{
-		'name': 'South Church'
-		'id': 'church'
+		'name': 'South Church',
+		'id': 'church',
 		'street': 'southside'
 	},
 	{
-		'name': 'St. Mary\'s Hospital'
-		'id': 'hospital'
+		'name': 'St. Mary\'s Hospital',
+		'id': 'hospital',
 		'street': 'uptown'
 	},
 	{
-		'name': 'The Unnamable'
-		'id': 'unnamable'
+		'name': 'The Unnamable',
+		'id': 'unnamable',
 		'street': 'merchant'
 	},
 	{
-		'name': 'The Witch House'
-		'id': 'witch'
+		'name': 'The Witch House',
+		'id': 'witch',
 		'street': 'blue'
 	},
 	{
-		'name': 'Train Station'
-		'id': 'train'
+		'name': 'Train Station',
+		'id': 'train',
 		'street': 'northside'
 	},
 	{
-		'name': 'Unvisited Isle'
-		'id': 'unvisited'
+		'name': 'Unvisited Isle',
+		'id': 'unvisited',
 		'street': 'merchant'
 	},
 	{
-		'name': 'Velma\'s Diner'
-		'id': 'velma'
+		'name': 'Velma\'s Diner',
+		'id': 'velma',
 		'street': 'easttown'
 	},
 	{
-		'name': 'Woods'
-		'id': 'woods'
+		'name': 'Woods',
+		'id': 'woods',
 		'street': 'uptown'
 	},
 	{
-		'name': 'Ye Olde Magick Shoppe'
-		'id': 'magick'
+		'name': 'Ye Olde Magick Shoppe',
+		'id': 'magick',
 		'street': 'uptown'
 	}
-}
+];
