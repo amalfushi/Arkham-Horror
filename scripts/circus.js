df2ed838ecf5cb4d1af694d3335247a2883ca4e8
David Dugan <DavidADugan@gmail.com>
2015-01-15 23:57:23 -0600

    added variable for circus.js, this is for short term testing purposes

--- scripts/circus.js
+++ scripts/circus.js
@@ -1,4 +1,4 @@
-{
+var circus = [
 	{
 		'title': 'Mirror Maze',
 		'body': "Wandering through the Mirror Maze you find yourself laughing at the characiture's of yourself that the mirrors reveal. That is until you realize that some of these images seem to not only be different shapes than normal, but also seem to wear slightly different clothing and bear slightly different scars. As if they were not contorted mirrors but rather other versions of yourself, other possibilities."
@@ -9,4 +9,4 @@
 		'decide': true,
 		'decide-text': 'Do you rush in?'
 		'agree-text': 'You rush in to the red-tinted darkness and find that the carny running the ride has run in right beside you. Inside you two confront a misshapen beast that has cornered a young woman and her beau. Fight a monster. If you win, take Samson the Strong Man as a follower'
-	},
+	}];
