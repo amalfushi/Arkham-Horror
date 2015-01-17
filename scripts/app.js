e59c4e2bc1ff51c135e81ba7556158ce93e070c1
David Dugan <DavidADugan@gmail.com>
2015-01-16 22:52:59 -0600

    Reading encounters from array

--- scripts/app.js
+++ scripts/app.js
@@ -6,16 +6,12 @@
 
       $scope.encounter = null;
 
-      $scope.circus = [{
-        'title': 'Mirror Maze',
-        'body': "Wandering through the Mirror Maze you find yourself laughing at the characiture's of yourself that the mirrors reveal. That is until you realize that some of these images seem to not only be different shapes than normal, but also seem to wear slightly different clothing and bear slightly different scars. As if they were not contorted mirrors but rather other versions of yourself, other possibilities."
-      }];
-
-      $scope.locations = locations; 
+      $scope.locations = locations;
       $scope.getEncounter = function(loc){
-        //event.preventDefault();
         $scope.locale = loc;
-        $scope.encounter = $scope.circus[0];
+        var location = eval(loc);
+        var enc = Math.floor(Math.random() * (location.length));
+        $scope.encounter = location[enc];
       }
   });
 })();
