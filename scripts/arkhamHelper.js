6fb942653a976d685145785031b5ca4517a573c1
David Dugan <DavidADugan@gmail.com>
2015-03-05 21:54:46 -0600

    Splitting up react components

--- scripts/arkhamHelper.js
+++ scripts/arkhamHelper.js
@@ -7,33 +7,3 @@ var DoomTrack = React.createClass({
   );
 }
 });
-
-var Locations = React.createClass({displayName:'Location',
-  render: function() {
-    return (
-      <div className='Location'>
-        Da Streets
-      </div>
-    );
-  }
-});
-
-var Mythos = React.createClass({
-  render: function() {
-    return (
-      <div className='Mythos'>
-        Mythos
-      </div>
-    );
-  }
-});
-
-var TerrorTrack = React.createClass({
-  render: function() {
-    return (
-      <div className='Terror'>
-        Terror
-      </div>
-    );
-  }
-});
